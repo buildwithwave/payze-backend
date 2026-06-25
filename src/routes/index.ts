96378e83779737943907501f69b2ddb83a67531e
Precious Kayili <preciouskayili@gmail.com>
2026-06-25 21:38:04 +0100

    feat: implement health check route and central router configuration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import healthRoutes from "./health.routes";
 
-const router = Router();
+const router: Router = Router();
 
 router.use("/health", healthRoutes);
 
